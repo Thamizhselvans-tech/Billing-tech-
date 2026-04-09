a5a420203107e041a8527f8d00948d1867f2313e
Thamizhselvans-tech <sthamizhselvan3@gmail.com>
2026-04-09 17:55:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const path = require('path');
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://billing-tech.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
