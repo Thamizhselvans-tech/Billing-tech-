94041bc6cb474c53a0692462b27dcd93709b30f1
Thamizhselvans-tech <sthamizhselvan3@gmail.com>
2026-04-09 17:42:55 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ 
+  baseURL: 'https://billing-tech.onrender.com/api'
+});
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('vb_token');
